b1ed5b663abfe463acc71888750b555429ab4f93
엄태호 <qqsscc7342@gmail.com>
2023-02-01 12:22:43 +0900

    refactor: 예약 테스트 리팩토링

--- src/main/java/nextstep/domain/dto/request/ReservationRequest.java
+++ src/main/java/nextstep/domain/dto/request/ReservationRequest.java
@@ -2,12 +2,14 @@ package nextstep.domain.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ReservationRequest {
     @Schema(description = "스케줄 아이디")
     private Long scheduleId;
